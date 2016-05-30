3d800ddbe9d1b35c6e7e687101fbeb5b8ce1abf7
naruto34 <naruto.34@hotmail.fr>
2016-05-30 20:41:24 +0200

    v0.6h

--- js/save_para.js
+++ js/save_para.js
@@ -3,5 +3,4 @@ function save_para(){
 	window.opener.document.getElementById("k").value = document.getElementById('k').value;
 	window.opener.document.getElementById("h").value = document.getElementById('h').value;
 	window.opener.document.getElementById("G").value = document.getElementById('G').value;
-	alert(window.opener.document.getElementById("c").value);
 }
